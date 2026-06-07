ab9084731b1c4853f17cb1796946595846bf5b3e
Nonoob123 <120779051+Nonoob123@users.noreply.github.com>
2026-06-07 08:09:53 +0800

    V2.1
    V2.1

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/StoryBook/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
